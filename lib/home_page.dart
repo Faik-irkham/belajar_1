8c995446c49bc17586e081829b4976d279bd225f
Faik-irkham <faikirkham@gmail.com>
2024-09-23 21:29:39 +0700

    init project

---
+++ lib/home_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+          color: Colors.amber,
+          child: const Text(
+            'Ini Home Page',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
